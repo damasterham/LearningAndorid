ece8a1ffb68352ded98589e08b399a42c67e66b6
damasterham <cmeisterham@gmail.com>
2017-11-30 15:43:50 +0100

    Started adding logic for db operations and ui binding, starting making a service for it, but enden up just trying to make it work with seperate threads

--- app/src/main/java/damasterham/learningandroid/MainActivity.java
+++ app/src/main/java/damasterham/learningandroid/MainActivity.java
@@ -1,10 +1,25 @@
 package damasterham.learningandroid;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.text.Layout;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+
+import damasterham.learningandroid.data.AppDb;
+import damasterham.learningandroid.data.dao.DudeDao;
+import damasterham.learningandroid.data.entitiy.Dude;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final String DB_NAME = "sampledb";
+
+    private AppDb mAppDb;
+    private DudeDao mDudeDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,5 +27,63 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Build DB
+        mAppDb = Room.databaseBuilder(this, AppDb.class, DB_NAME).build();
+        mDudeDao = mAppDb.dudeDao();
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Initialize some dudes
+                if (mDudeDao.getAll().size() == 0)
+                {
+                    mDudeDao.insertAll(new Dude("Bente", "SuperBente"),
+                            new Dude("Jens", "HammerJens"),
+                            new Dude("Erik", "ErikVonPopidan"));
+                    //Log.d("Dudes created", "onCreate: added " + mDudeDao.getAll().toString());
+                }
+            }
+        }).start();
+
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                List<Dude> dudes = mDudeDao.getAll();
+
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+//                        TextView tv = findViewById(R.id.hellotext);
+//                        tv.setText("");
+//
+//                        for (Dude dude: dudes)
+//                        {
+//                            tv.append(dude.getName());
+//                            tv.append("\n");
+//                            tv.append(dude.getNickName());
+//                            tv.append("\n\n");
+//                        }
+                    }
+                });
+
+
+            }
+        }).start();
+    }
+
 }
+
+
